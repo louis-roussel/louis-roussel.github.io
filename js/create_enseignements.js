5b9aacf23d43abb0a97e9a2561b83495a8bdf5c5
Louis Roussel <louis.roussel@univ-lille.fr>
2024-05-18 21:56:18 +0200

    enseignements

---
+++ js/create_enseignements.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+function group_enseign_by_year(comms){
+    const grouped =  group_list_by_filter(comms, "year")
+    return dict_to_sorted_list_by_key_value(grouped)
+}
+
+
+
+
+async function create_enseignements_div() { 
+    const enseign_by_year_sorted = group_enseign_by_year(all_enseignements);
+  
+    // $("#enseignements").append("<pre>"+JSON.stringify(enseign_by_year_sorted,null,'\t')+"</pre>");  
+
+    enseign_by_year_sorted.forEach(e => {
+        let year = e[0];
+        let enseign_per_year = e[1];
+        let div_year = $('<div class="enseign_year_div"></div>');
+        div_year.append('<h3 class="enseign_year">'+year+'</h3>');
+
+        enseign_per_year.forEach(enseign => {
+            
+
+            let div_enseign = $('<ul class="enseign list-group"></ul>');
+            let detail_htd = 0;
+            let title_htd = enseign.title +" " + '<span class="badge text-bg-primary rounded-pill">'+enseign.HTD+' HTD</span>'
+            // <li class="list-group-item d-flex justify-content-between align-items-center">
+            //     A list item
+            //     <span class="badge text-bg-primary rounded-pill">14</span>
+            // </li>
+
+
+            div_enseign.append('<p class = "enseign_title">'+ title_htd +'</p>');
+            div_enseign.append('<i class = "enseign_where">'+ enseign.study_year+", "+enseign.where +'</i>'); 
+            // div_comm.append('<p class = "comm_where">'+ comm.where +'</p>'); 
+            // div_comm.append('<p class = "comm_where">'+ formatted_date +'</p>');
+            div_year.append(div_enseign);
+        });
+
+        $("#enseignements").append(div_year);
+    });
+
+}
+
+// Call the async function to fetch and log the data
+create_enseignements_div();
---
+++ js/data/enseignements.js
@@ -0,0 +1,101 @@
+const all_enseignements =  [
+    {
+        "title": "Programmation numérique",
+        "year": 2022,
+        "study_year":"IS3",
+        "where":"Polytech Lille",
+        "HTD":46,
+        "heures":{
+            "CM":0,
+            "CTD":8,
+            "TD":0,
+            "TP":36,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Machine Learning",
+        "year": 2022,
+        "study_year":"IS4",
+        "where":"Polytech Lille",
+        "HTD":18,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":0,
+            "TP":10,
+            "Projet":10
+        }
+    },
+    {
+        "title": "Programmation numérique",
+        "year": 2023,
+        "study_year":"IS3",
+        "where":"Polytech Lille",
+        "HTD":16,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":0,
+            "TP":16,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Algèbre linéaire numérique",
+        "year": 2023,
+        "study_year":"IS3",
+        "where":"Polytech Lille",
+        "HTD":20,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":0,
+            "TP":20,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Base de données",
+        "year": 2023,
+        "study_year":"PEIP2",
+        "where":"Polytech Lille",
+        "HTD":21,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":9,
+            "TP":12,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Encadrement mémoire de master",
+        "year": 2023,
+        "study_year":"Master Machine Learning",
+        "where":"Université de Lille",
+        "HTD":2,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":0,
+            "TP":0,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Encadrement projet de master",
+        "year": 2023,
+        "study_year":"Master Machine Learning",
+        "where":"Université de Lille",
+        "HTD":2,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":0,
+            "TP":0,
+            "Projet":0
+        }
+    },
+];
+  
